0382e15591f3d6f94416fd0ec3b28b7ab26f9c88
Kra Larivain <olarivain@gmail.com>
2015-06-09 19:25:14 -0700

    carthage fix

---
+++ MobileAppTracker/MobileAppTrackerFramework.h
@@ -0,0 +1,19 @@
+//
+//  MobileAppTracker.h
+//  MobileAppTracker
+//
+//  Created by Olivier Larivain on 6/9/15.
+//  Copyright (c) 2015 hasOffer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MobileAppTracker.
+FOUNDATION_EXPORT double MobileAppTrackerVersionNumber;
+
+//! Project version string for MobileAppTracker.
+FOUNDATION_EXPORT const unsigned char MobileAppTrackerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MobileAppTracker/PublicHeader.h>
+
+
